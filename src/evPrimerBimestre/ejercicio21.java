1ea6bc7e0fbf9a57ec43266a2668c65e9251cb7a
Your Name <you@example.com>
2025-04-11 10:43:32 -0300

    subo ejercicio 21

---
+++ src/evPrimerBimestre/ejercicio21.java
@@ -0,0 +1,33 @@
+package evPrimerBimestre;
+
+public class ejercicio21 {
+
+	public static void main(String[] args)throws InterruptedException  {
+	
+		int bateria = 100;
+		
+		
+		while(bateria >= 0) {
+			
+			System.out.println("bateria:"+ bateria+"%");
+			
+			Thread.sleep(20);
+			
+			if(bateria==20) {
+				
+			System.out.println("bateria baja");
+			
+			Thread.sleep(1000);
+				
+			}
+			
+			bateria--;
+		}
+		
+		System.out.println("bateria agotada");
+		
+		
+
+	}
+
+}
